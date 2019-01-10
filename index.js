b97410b531521cbb46f65aff815fdad3bb120bb3
olaven <olavsundfoer@gmail.com>
2019-01-10 13:16:09 +0100

    working on incorporating tree-lib

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const mkdirp = require('mkdirp'); 
+const Arboreal = require('arboreal'); 
 
-const assign = array => {
-    return (array ? array : []);
+const tree = new Arboreal(); 
+
+
+tree
+    .appendChild("super1")
+    .appendChild("super2")
+    .children[0]
+        .appendChild("sub1sup2")
+        .appendChild("sub2sup2"); 
+   
+
+/**
+ * Returns the first node that matches 
+ * the regex pattern 
+ * @param {string} pattern 
+ */
+const find = (pattern) => {
+    return tree.find(node => {
+        const regex = new RegExp(pattern); 
+        return regex.test(node.data); 
+    })
 }
+
+
 
 
 const directory = () => {
@@ -10,28 +32,37 @@ const directory = () => {
     //callback receives directory 
     this.create = (name, callback) => {
         // create structure if this is first `create`
-        this.structure = assign(this.structure);
+        this.tree = (this.tree? this.tree: tree) 
 
         //i.e. has parent-create with name 
         if (this.superdir) { // TODO: Add support for more than one subdir (use tree or similar, I think. Has to be searchable to enter at correct place)
-            this.structure[this.superdir] = assign(this.structure[this.superdir]); 
-            this.structure[this.superdir].push(name); 
+            // find location of superdir int tree 
+            // add name under that 
+            let node = find("sub2sup2");
+            node.appendChild(name); 
         } else {
-            this.structure[name] = []; 
+            // append on root in tree 
+            tree.appendChild(name); 
         }  
         
         if (callback) {
             this.superdir = name;
             callback(this)
-        } else {
+        }/* else { I believe this is a mistake -> when in sub, I do just have the same superdir 
             this.superdir = undefined;
-        }
+        }*/
 
         return this;  
     }
 
     this.now = () => {
-        console.log("creating dir with:", this.structure); 
+        // console.log("creating dir with:", this.tree);
+        /*
+        const iterator = (node) => {
+            // console.log(node);  
+        }
+
+        tree.traverseDown(iterator);  */
     }
 
     return this; 
@@ -39,12 +70,13 @@ const directory = () => {
 
 directory()
     .create("hello")
-    .create("super", (directory) => {
+    .create("school", (directory) => {
         directory
-            .create("subsuper", (directory) => {
+            .create("algorithms", (directory) => {
                 directory 
-                    .create("subsub")
+                    .create("notes")
             })
+            .create("programming")
     })
     .now(); 
 
