35d25a8d3f407b4b4ea3f7d25db53bf15c5ff5b4
olaven <olavsundfoer@gmail.com>
2019-01-13 13:08:21 +0100

    changed API and finally have something easier and that actually works.

--- README.md
+++ README.md
@@ -1,46 +1,44 @@
 # make-directories
 Micro-library for easy directory creation. 
 
+## Usage 
+```javascript 
+const structure = {
+    "books": {
+        "fiction": {}
+        "non-fiction": {}
+    }, 
+    "school": {
+        "programming": {
+            "notes": {}, 
+            "examples": {}
+        }, 
+        "databases": {}, 
+        "alorithms": {}
+    }, 
+    "other": {}
+}
+
+mkdir(structure); 
+```
+
+Will produce the following directory structure: 
+* books
+  * fiction
+  * non-fiction
+* school
+  * programming
+    * notes
+    * examples
+  * databases
+  * algorithms
+* other
+
+
 ## TODO: 
-- [ ] Make the API work
+- [X] Make the API work
 - [ ] Spice up readme 
 - [ ] Make repo public 
 - [ ] Publish module 
-- [ ] Typescript-definition, use dts-gen
-- [ ] Refactoring and clean-up
+- [X] Refactoring and clean-up
 - [ ] Build in custom directory
-
-## Usage 
-```javascript 
-directory()
-    .create("school", (directory) => {
-        directory
-            .create("programming")
-            .create("algorithms")
-            .create("databases")
-    })
-    .create("hobbies", (directory) => {
-        directory
-            .create("books", (directory) => {
-                directory
-                    .create("fiction")
-                    .create("non-fiction")
-            })
-            .create("movies")
-            .create("cooking")
-    })
-    .create("other")
-```
-
-Will (when package is finished) produce the following directory structure: 
-* school
-  * programming
-  * algorithms
-  * databases
-* hobbies
-  * books
-    * fiction
-    * non-fiction
-  * movies
-  * cooking
-* other
--- index.js
+++ index.js
@@ -1,130 +1,13 @@
 const mkdirp = require('mkdirp'); 
-const Arboreal = require('arboreal'); 
 
-const tree = new Arboreal(); 
-
-
-
-/**
- * Returns the first node that matches 
- * the regex pattern 
- * @param {string} pattern 
- */
-const find = (pattern) => {
-    return tree.find(node => {
-        const regex = new RegExp(pattern); 
-        return regex.test(node.data); 
-    })
-}
-
-/**
- * Returns the last element in array
- */
-Array.prototype.peek = function() {
-    if (this.length <= 0) return null; 
-    return this[this.length - 1]; 
-}
-
-/**
- * Returns true if the array contains 
- * all node in given array based on their data 
- */
-Array.prototype.containsAllIn = function(array) {
-
-    return array.filter(node => 
-        this.find(n => n.data === node.data) !== undefined
-    ).length === array.length; 
-}
-
-
-// The tree generation is faulty! 
-const directory = () => {
-    this.currentPath = []; 
-    //callback receives directory 
-    this.create = (name, callback) => {
-        // create structure if this is first `create`
-        this.tree = (this.tree? this.tree: tree) 
-
-        //i.e. is going down in a path 
-        if (this.currentPath.length > 0) { 
-            // find location of currentPath int tree 
-            // add name under that 
-            let currentSuperDir = this.currentPath.peek(); 
-            let node = find(currentSuperDir);
-            this.depth = node.depth; 
-            node.appendChild(name); 
-        } else {
-            // append on root in tree 
-            tree.appendChild(name); 
-            this.depth = 0; 
-        }  
-        
-        if (callback) {
-            this.currentPath.push(name); 
-            callback(this)
-        } else {
-            this.currentPath.pop();
-        }
-
-        return this;  
-
+let path = []; 
+const mkdir = (structure) => {
+    for(key of Object.keys(structure)) {
+        path.push(key); 
+        mkdirp(path.join("/")); 
+        mkdir(structure[key]); 
     }
-
-    this.now = () => {
-
-        // Below code just for debuging 
-        const printIterator = (node) => {
-            console.log("-".repeat(node.depth) + "> " + node.data)
-        };
-        this.tree.traverseDown(printIterator);
-        // Above code just for debuging
-
-        let path = [];
-        const iterator = (node) => {
-            // dersom chlidren.length == 0 -> bunn, bygg path med parent-linker 
-            // Fortsett (gjøres automatisk gjennom iterator) 
-            if (node.children.length === 0) {
-                let current = node; 
-                while(current !== undefined && current !== null) {
-                    path.push(current.data); 
-                    current = current.parent; 
-                }
-                path.pop(); // remove top-object
-                let pathString = path.reverse().join("/"); 
-                mkdirp(pathString);
-                path = []; 
-            }
-        } 
-
-        this.tree.traverseDown(iterator); 
-        
-    }
-
-    return this; 
+    path.pop(); 
 }
 
-// CALLING FOR TESTING
-directory()
-    .create("super", (directory) => {
-        directory
-            .create("school", (directory) => {
-                directory
-                    .create("programming")
-                    .create("algorithms")
-                    .create("databases")
-            })
-            .create("hobbies", (directory) => {
-                directory
-                    .create("books", (directory) => {
-                        directory
-                            .create("fiction")
-                            .create("non-fiction")
-                    })
-                    .create("movies")
-                    .create("cooking")
-            })
-            .create("other")
-    })
-    .now(); 
-
-module.exports = directory; 
+module.exports = mkdir; 
